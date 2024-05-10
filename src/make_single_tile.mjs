abd74d9d7852f36afc8e29232531071ed946514b
aborzym <borzym.a@gmail.com>
2024-05-10 09:09:38 +0200

    make single tile js - 02

--- src/make_single_tile.mjs
+++ src/make_single_tile.mjs
@@ -19,11 +19,12 @@ const makeGenresString = (array) => {
   let genres; //tablica obiektów z IDs
   getGenres().then((data) => {
     genres = data;
+    array.forEach((val) => {
+      const element = genres.find((gen) => gen.id === val);
+      gensArray.push(element.name);
+    });
+    return gensArray.join(', ');
   });
-  genres.forEach((val) => {
-    gensArray.push(genres.find((gen) => gen.id === val).name);
-  });
-  return gensArray.join(', ');
 };
 
 //funkcja generująca pojedynczy kafelek filmu
@@ -38,8 +39,28 @@ export const makeSingleFilmTile = (film) => {
     film.poster_path
   }" alt="${film.title} poster"/></div>
   <div class="film-info>
-  <p class="film-title">${film.title.toUppercase()}</p>
+  <p class="film-title">${film.title.toUpperCase()}</p>
   <p class="film-details">${genres} | ${year}</p>
   </div>
   </li>`;
 };
+
+const movie = {
+  adult: false,
+  backdrop_path: '/8YURPHUqD2ODIffxPf8sG5l5UC7.jpg',
+  genre_ids: [12, 36, 10752, 28],
+  id: 652,
+  original_language: 'en',
+  original_title: 'Troy',
+  overview:
+    'In year 1250 B.C. during the late Bronze age, two emerging nations begin to clash. Paris, the Trojan prince, convinces Helen, Queen of Sparta, to leave her husband Menelaus, and sail with him back to Troy. After Menelaus finds out that his wife was taken by the Trojans, he asks his brother Agamemnon to help him get her back. Agamemnon sees this as an opportunity for power. They set off with 1,000 ships holding 50,000 Greeks to Troy.',
+  popularity: 82.284,
+  poster_path: '/a07wLy4ONfpsjnBqMwhlWTJTcm.jpg',
+  release_date: '2004-05-13',
+  title: 'Troy',
+  video: false,
+  vote_average: 7.16,
+  vote_count: 9817,
+};
+
+console.log(makeSingleFilmTile(movie));
