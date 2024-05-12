bb451d88458a0fb98c91214e41bc1fa90100b509
aborzym <borzym.a@gmail.com>
2024-05-12 22:27:45 +0200

    working

--- src/scripts/index.mjs
+++ src/scripts/index.mjs
@@ -1,18 +1,23 @@
 import { makeSingleFilmTile } from './make_single_tile.mjs';
 import { getTrendingFilms } from './get-trending.mjs';
 import { getFilmsByQueryString } from './get-films-by-query.mjs';
+
 // GET TRENDING
 
 const gallery = document.querySelector('ul.gallery');
 
-getTrendingFilms().then(async (data) => {
-  gallery.innerHTML = '';
-  const results = data.results;
-  for (let i = 0; i < results.length; i++) {
-    const newTile = await makeSingleFilmTile(results[i]);
-    gallery.insertAdjacentHTML('beforeend', newTile);
-  }
-});
+getTrendingFilms()
+  .then(async (data) => {
+    gallery.innerHTML = '';
+    const results = data.results;
+    for (let i = 0; i < results.length; i++) {
+      const newTile = await makeSingleFilmTile(results[i]);
+      gallery.insertAdjacentHTML('beforeend', newTile);
+    }
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 
 //GET FILMS BY QUERY STRING
 
@@ -21,6 +26,17 @@ const searchForm = document.querySelector('form.search-form');
 searchForm.addEventListener('submit', async (ev) => {
   ev.preventDefault();
   const queryString = ev.target.elements.searchinput.value;
-  const getFilms = await getFilmsByQueryString(queryString);
   gallery.innerHTML = '';
+  getFilmsByQueryString(queryString)
+    .then(async (data) => {
+      gallery.innerHTML = '';
+      const results = data.results;
+      for (let i = 0; i < results.length; i++) {
+        const newTile = await makeSingleFilmTile(results[i]);
+        gallery.insertAdjacentHTML('beforeend', newTile);
+      }
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 });
