da25b220d3bfde63d87857e3864ee78e063485ff
aborzym <borzym.a@gmail.com>
2024-05-12 11:45:51 +0200

    final

--- src/scripts/index.mjs
+++ src/scripts/index.mjs
@@ -2,11 +2,11 @@ import { makeSingleFilmTile } from './make_single_tile.mjs';
 import { getTrendingFilms } from './get-trending.mjs';
 
 const gallery = document.querySelector('ul.gallery');
-getTrendingFilms().then((data) => {
+getTrendingFilms().then(async (data) => {
   gallery.innerHTML = '';
   const results = data.results;
   for (let i = 0; i < results.length; i++) {
-    console.log(results[i]);
-    gallery.insertAdjacentHTML('beforeend', makeSingleFilmTile(results[i]));
+    const newTile = await makeSingleFilmTile(results[i]);
+    gallery.insertAdjacentHTML('beforeend', newTile);
   }
 });
