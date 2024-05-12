94518c635644a833a71001640c13019be214920c
aborzym <borzym.a@gmail.com>
2024-05-12 10:51:37 +0200

    05

--- src/scripts/index.mjs
+++ src/scripts/index.mjs
@@ -2,11 +2,11 @@ import { makeSingleFilmTile } from './make_single_tile.mjs';
 import { getTrendingFilms } from './get-trending.mjs';
 
 const gallery = document.querySelector('ul.gallery');
-
 getTrendingFilms().then((data) => {
   gallery.innerHTML = '';
   const results = data.results;
   for (let i = 0; i < results.length; i++) {
-    gallery.insertAdjacentElement(makeSingleFilmTile(results[i]));
+    console.log(results[i]);
+    gallery.insertAdjacentHTML('beforeend', makeSingleFilmTile(results[i]));
   }
 });
