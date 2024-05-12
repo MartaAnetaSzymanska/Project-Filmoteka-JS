1fdf7310884b392f9f397fe89e92a016eda3bf3e
aborzym <borzym.a@gmail.com>
2024-05-12 10:05:59 +0200

    04

--- src/scripts/get-trending.mjs
+++ src/scripts/get-trending.mjs
@@ -1,12 +1,11 @@
 import axios from 'axios';
 import { apiKey } from './make_single_tile.mjs';
 
-const gallery = document.querySelector('ul.gallery');
 const trendingQuery = `https://api.themoviedb.org/3/trending/movie/day?api_key=${apiKey}`;
 
 //UWAGA - wywoływać z await!!!!
 
-const getTrendingFilms = async () => {
+export const getTrendingFilms = async () => {
   return await axios.get(trendingQuery).then((response) => {
     return response.data;
   });
--- src/scripts/index.mjs
+++ src/scripts/index.mjs
@@ -1,6 +1,12 @@
 import { makeSingleFilmTile } from './make_single_tile.mjs';
 import { getTrendingFilms } from './get-trending.mjs';
 
+const gallery = document.querySelector('ul.gallery');
+
 getTrendingFilms().then((data) => {
-  console.log(data);
+  gallery.innerHTML = '';
+  const results = data.results;
+  for (let i = 0; i < results.length; i++) {
+    gallery.insertAdjacentElement(makeSingleFilmTile(results[i]));
+  }
 });
