40c43274034ab96a0f84df25f2b2f44b4f763477
MartaAnetaSzymanska <150442037+MartaAnetaSzymanska@users.noreply.github.com>
2024-05-13 10:02:47 +0000

    Deploying to gh-pages from @ MartaAnetaSzymanska/Project-Filmoteka-JS@c86de7697f4d368109d3b772fdadf9edd1f0a5d7 🚀

---
+++ index.33e687ab.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("ul.gallery"),t=document.querySelector("[data-modal]");document.querySelector("[modal-close]");function c(){t.classList.toggle("is-hidden")}e.addEventListener("click",c),t.addEventListener("click",c),e.addEventListener("click",(function(e){var t=e.target.id;console.log(t)}))}();
+//# sourceMappingURL=index.33e687ab.js.map
---
+++ index.4fa5dfc1.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("ul.gallery"),t=document.querySelector("[data-modal]");document.querySelector("[modal-close]");function c(){t.classList.toggle("is-hidden")}e.addEventListener("click",c),t.addEventListener("click",c),e.addEventListener("click",(e=>{const t=e.target.id;console.log(t)}));
+//# sourceMappingURL=index.4fa5dfc1.js.map
