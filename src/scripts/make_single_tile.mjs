a2d1ecf7e1940d5923f94f7bc2e9e211fe085e9e
Seweryn <sewcio0212@gmail.com>
2024-05-14 14:16:09 +0200

    class-names changed

--- src/scripts/make_single_tile.mjs
+++ src/scripts/make_single_tile.mjs
@@ -33,13 +33,35 @@ export const makeSingleFilmTile = async (film) => {
   const genres = await makeGenresString(film.genre_ids); //film.genre_ids to tablica IDs gatunków
 
   const year = film.release_date.slice(0, 4);
-  return `<li class="movie-container__card">
-            <div class="poster"><img class="poster__img" src="${moviePoster}" alt="${title} poster" /></div>
-            <div class="movieInfo">
-               <p class="movieInfo__item movieInfo--title">${movieName}</p>
-               <p class="movieInfo__item">
-                     ${matchedGenres} | ${movieDate}
-              </p>
-            </div>
-          </li>`;
+  return `<li class="movie-block">
+  <div class="image"><img class="image__img" src="https://image.tmdb.org/t/p/original${
+    film.poster_path
+  }" alt="${film.title} poster"/></div>
+  <div class="film-info" id="${film.id}">
+  <p class="film-info__item film-info--title">${film.title.toUpperCase()}</p>
+  <p class="film-info__item">${genres} | ${year}</p>
+  </div>
+  </li>`;
 };
+
+//poniższe wrzucone dla sprawdzenia, jak potem wszystko będzie działać to do usunięcia
+
+// const movie = {
+//   adult: false,
+//   backdrop_path: '/8YURPHUqD2ODIffxPf8sG5l5UC7.jpg',
+//   genre_ids: [12, 36, 10752, 28],
+//   id: 652,
+//   original_language: 'en',
+//   original_title: 'Troy',
+//   overview:
+//     'In year 1250 B.C. during the late Bronze age, two emerging nations begin to clash. Paris, the Trojan prince, convinces Helen, Queen of Sparta, to leave her husband Menelaus, and sail with him back to Troy. After Menelaus finds out that his wife was taken by the Trojans, he asks his brother Agamemnon to help him get her back. Agamemnon sees this as an opportunity for power. They set off with 1,000 ships holding 50,000 Greeks to Troy.',
+//   popularity: 82.284,
+//   poster_path: '/a07wLy4ONfpsjnBqMwhlWTJTcm.jpg',
+//   release_date: '2004-05-13',
+//   title: 'Troy',
+//   video: false,
+//   vote_average: 7.16,
+//   vote_count: 9817,
+// };
+
+// console.log(await makeSingleFilmTile(movie));
