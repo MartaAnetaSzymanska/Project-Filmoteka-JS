e508f50ed27fa01748b108d51cedceae3feef0ab
aborzym <borzym.a@gmail.com>
2024-05-14 10:10:52 +0200

    done

--- src/scripts/get-films-by-query.mjs
+++ src/scripts/get-films-by-query.mjs
@@ -1,10 +1,10 @@
 import axios from 'axios';
 import { apiKey } from './make_single_tile.mjs';
 
-export const getFilmsByQueryString = async (string) => {
+export const getFilmsByQueryString = async (string, page) => {
   return await axios
     .get(
-      `https://api.themoviedb.org/3/search/movie?query=${string}&api_key=6822ab68b639c8d7f457546b90aae724`
+      `https://api.themoviedb.org/3/search/movie?query=${string}&api_key=6822ab68b639c8d7f457546b90aae724&page=${page}`
     )
     .then((response) => {
       return response.data;
